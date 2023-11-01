c6a3658738429ccbf09ea47b820f470a591c15f4
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-11-01 13:05:53 -0700

    Deploy task 3

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,5 +1,15 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_insert_right - insert a newnode to the right
+ * of a given tree node
+ *
+ * @parent: pointer to parent node
+ * @value: data value to assign new node
+ *
+ * Return: ponter to added node
+ */
+
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	if (parent == NULL)
@@ -10,7 +20,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (newNode == NULL)
 		return (NULL);
 
-	if(parent->right != NULL)
+	if (parent->right != NULL)
 		parent->right->parent = newNode;
 	newNode->right = parent->right;
 	parent->right = newNode;
