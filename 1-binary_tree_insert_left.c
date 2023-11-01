830ddfb3da353365122bd4be5b3ea751b86a7b1a
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-11-01 08:36:39 -0700

    Refix 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,17 +18,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	if (parent == NULL)
 		return (NULL);
-	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+	
+	binary_tree_t *newNode =  binary_tree_node(parent, value);
 
 	if (newNode == NULL)
 		return (NULL);
-	newNode->parent = parent;
-	newNode->n = value;
-	newNode->right = NULL;
-	newNode->left = parent->left;
+
 	if (parent->left != NULL)
+	{
+		newNode->left = parent->left;
 		parent->left->parent = newNode;
-	parent->left = newNode;
+	}
 
 	return (newNode);
 }
