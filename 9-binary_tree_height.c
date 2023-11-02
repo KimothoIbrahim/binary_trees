0fe88bf8c656eb231c2fc3b9c4d2d1ab9fc5f256
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-11-02 16:54:49 -0700

    Compare

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,15 +10,12 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	else 
+	if (tree)
 	{
-		size_t length_left = binary_tree_height(tree->left);
-		size_t length_right = binary_tree_height(tree->right);
-		if (length_left > length_right)
-			return (length_left + 1);
-		else
-			return (length_right + 1);
+		size_t leftH = 0, rightH = 0;
+		leftH = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		rightH = tree-> right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((leftH > rightH) ? leftH : rightH);
 	}
+	return (0);
 }
